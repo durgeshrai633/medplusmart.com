8799d9198a234fe478cb06337a6577d0753f09c1
kosachin <80893464+kosachin@users.noreply.github.com>
2021-11-04 13:50:33 +0530

    added autoplay to carousel

--- promotion_page_sachin/promotion.js
+++ promotion_page_sachin/promotion.js
@@ -2,9 +2,9 @@
 var carouselImges = ["https://static2.medplusmart.com/live/bannerImage/Mart/0036e89c7e5a2972f3061b54d182f722.jpg",
     "https://static2.medplusmart.com/live/bannerImage/Mart/60343147c35ebd43bda28c383dad957e.jpg",
     "https://static2.medplusmart.com/live/bannerImage/Mart/362fcf14bcdb8e04a481cc1c29bc99ea.jpg",
-    "https://static2.medplusmart.com/live/bannerImage/Mart/6c819bc37cfa2c512bc929db636c79d2.jpg"
+    "https://static2.medplusmart.com/live/bannerImage/Mart/6c819bc37cfa2c512bc929db636c79d2.jpg",
 ]
-
+// To load the carousel start
 var caroTrack = document.querySelector(".carousel-track");
 
 carouselImges.map(function (item) {
@@ -15,43 +15,93 @@ carouselImges.map(function (item) {
     li.append(img);
     caroTrack.append(li)
 })
-firstCarousel = caroTrack.children[0];
+var firstCarousel = caroTrack.children[0];
 firstCarousel.classList.add("current-slide")
+// To load the carousel end
 
+// To load carousel indicators start
+var indicatorNav = document.querySelector(".carousel-nav")
+carouselImges.map(function(){
+    var btn = document.createElement("button")
+    btn.setAttribute("class","carousel-indicator")
+    indicatorNav.append(btn)
+})
+var firstIndicator = indicatorNav.children[0];
+firstIndicator.classList.add("current-slide")
+// To load carousel indicators end 
 
+// To make carousel transitions start
 const track = document.querySelector(".carousel-track");
 const slides = Array.from(track.children)
-// console.log(track)
 const barsNav = document.querySelector(".carousel-nav");
 const bars = Array.from(barsNav.children);
-const slideWidth = slides[0].getBoundingClientRect().width;
-// console.log(barsNav)
+const slideWidth = slides[0].clientWidth;
+
+// To add offset to carousel images position start
 function setSlidePosition(slide, index) {
     slide.style.left = slideWidth * index + "px";
 }
 slides.forEach(setSlidePosition)
+// To add offset to carousel images position end
 
 function moveToSlide(track, currentSlide, targetSlide) {
     const nextSlide = targetSlide
-    // console.log(currentSlide, targetSlide)
+    track.style.transition = "transform 250ms ease-in"
     track.style.transform = `translateX(-${nextSlide.style.left})`
     currentSlide.classList.remove("current-slide");
     targetSlide.classList.add("current-slide");
 }
+// To make carousel transitions end
 
-barsNav.addEventListener("click", function (e) {
-    const targetBar = e.target.closest("button");
+// To make carousel indicator  transitions start
+function moveIndicator(e) {
+    const targetBar = e.target;
 
     const currentSlide = track.querySelector(".current-slide")
     const currentBar = barsNav.querySelector(".current-slide")
     const targetIndex = bars.findIndex(bar => bar === targetBar);
     const targetSlide = slides[targetIndex];
-    // console.log(targetSlide)
 
     moveToSlide(track, currentSlide, targetSlide)
     currentBar.classList.remove("current-slide")
     targetBar.classList.add("current-slide")
-})
+}
+bars.map((item) => item.addEventListener("click", function (e) {
+    moveIndicator(e)
+}))
+// To make carousel indicator  transitions end
+
+// To autoplay carousel start
+setInterval(function () {
+    autoPlay()
+}, 6000)
+
+function autoPlay() {
+    var currentBar = barsNav.querySelector(".current-slide")
+    var targetBar = currentBar.nextElementSibling;
+    var targetIndex = bars.findIndex(bar => bar === targetBar);
+    if(targetIndex !== -1) {
+        var currentSlide = track.querySelector(".current-slide")
+        var targetSlide = currentSlide.nextElementSibling;
+    
+    
+        moveToSlide(track, currentSlide, targetSlide)
+        currentBar.classList.remove("current-slide")
+        targetBar.classList.add("current-slide")
+    }
+    else{
+        var currentSlide = track.querySelector(".current-slide");
+        var targetSlide = track.children[0]
+        moveToSlide(track, currentSlide, targetSlide)
+        
+        var targetBar = barsNav.children[0]
+        currentBar.classList.remove("current-slide");
+        targetBar.classList.add("current-slide")
+    }
+
+}
+// To autoplay carousel end
+
 // carousel end
 
 
