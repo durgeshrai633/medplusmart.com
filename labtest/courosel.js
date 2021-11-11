dfa13e394b9e9d39965e7b9fa0f53ab13cc1f4e0
Akbar660 <shaikakbarbasha38@gmail.com>
2021-11-11 09:45:47 +0530

    changes in lab

--- labtest/courosel.js
+++ labtest/courosel.js
@@ -55,9 +55,14 @@ var userdata=JSON.parse(localStorage.getItem("userList"))
 var user=localStorage.getItem("user")
 // console.log(user)
 
-// for(var i=0;i<userdata.length;i++){
-    // if(userdata[i].userName==user){
+for(var i=0;i<userdata.length;i++){
+    if(userdata[i].userName==user){
         sign.innerHTML=`${user}
-        <i style="font-size:24px" class="fa">&#xf2be;</i>`;  
-    //  }
-// }
+        <i style="font-size:24px" class="fa">&#xf2be;</i>`;
+         
+sign.style.color="black";
+sign.style.fontSize="16px"
+sign.style.border="none";
+
+     }
+}
